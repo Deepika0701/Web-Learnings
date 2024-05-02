67821a5496360a0c29ab4a36f660b5ce867f087c
Deepika <82407112+Deepika0701@users.noreply.github.com>
2024-05-02 14:11:45 +0000

    operators

---
+++ Javascript/operators.js
@@ -0,0 +1,70 @@
+/*
+1. Assignment operators
+2. Comparison operators
+3. Arithmetic operators
+4. Bitwise operators
+5. Logical operators
+6. String operators
+7. Conditional operator
+8. Comma operator
+9. Unary operator
+10. Relational operator
+*/
+
+/* Assignment operator
+let x=10;
+let y;
+y=x;
+console.log(y); */
+
+/*Compound operators
+let x=10;
+let y=20;
+x=x+y;
+console.log(x);
+
+x+=y// we can write like this also*/
+
+// Arithmetic operators 
+//  +
+//  -
+//  *
+//  /
+//  **
+
+// Logical operators 
+// 1. Logical AND &&
+// 2. Logical OR ||
+// 3. Logical NOT !
+
+
+// const isLoggedIn =false;
+
+// if(!isLoggedIn){
+//     console.log('Redirect to login page');
+// }
+
+
+//Conditional operators
+
+// const userRole ='admin';
+
+// if(userRole==='admin'){
+//     console.log('you are  an admin');
+// }
+// else{
+//     console.log('you are not an admin');
+// }
+
+// userRole==='admin'?console.log('you are an admin'):console.log('not');
+
+
+// Comparison operators 
+// == equal (it checks only values) the process is called coercion(the data conversion )
+// != not equal 
+// === strict equal(it checks values and data types) here no coercion applied
+// !== strict not equal 
+// >
+// >=
+// <
+// <=

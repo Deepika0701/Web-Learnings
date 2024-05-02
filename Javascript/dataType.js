68c82f7dc41bd93069bd7d8573c95c076a624d22
Deepika <82407112+Deepika0701@users.noreply.github.com>
2024-05-02 13:36:52 +0000

    Data Types

---
+++ Javascript/dataType.js
@@ -0,0 +1,57 @@
+// I. Primitve data types 
+// The predefined data types provided by JavaScript language are known as primitive data types. Primitive data types are also known as in-built data types.
+
+// 1.Number 
+// let age=25;
+// let temp=-25;
+// let price=50.7;
+
+// 2.String 
+// let name='Rakesh'
+
+// 3.Boolean 
+// let isLoggedIn=true;
+// let hasPermission=false;
+
+//4.undefined(means declared but not initialized)
+// let age;
+// console.log(age);
+
+
+//5.Bigint
+// console.log(Number.MAX_SAFE_INTEGER);
+// its equivalent to
+// console.log(2**53 -1);
+// By using Big Int you can use the number greater than this number
+
+// 6.Symbol 
+// Unique
+
+
+
+
+// II. Non primitive data types(Structural type)
+// The data types that are derived from primitive data types of the JavaScript language are known as non-primitive data types. It is also known as derived data types or reference data types.
+
+// 1.Object 
+// 1.1 Function
+    // Non-data structure 
+    // callable
+// 1.2 Arrays
+    // collection of values     
+   //  const numbers=[2,3,4,'JS','PY',{name:'Rakesh'}];
+
+// 1.3 Maps 
+// 1.4 Set 
+// 1.5 Date 
+
+//III. Structural root
+// 1.null  
+
+      
+
+
+
+
+
+
